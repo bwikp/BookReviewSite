0b80b56af7fe3087bba259198aaada2d2007af74
bwikp <zilpa.michel@gmail.com>
2023-07-04 23:22:25 +0200

    first commit a DANS 66 jours

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import { onBeforeMount } from 'vue';
+import axios from 'axios';
+
+  const Allcategorie = ref([])
+  const fetchAllCatego = async ()=>{
+          const categorie = await axios.get('http://localhost:8000/api/categorie/4')
+          console.log(categorie.data)
+          Allcategorie.value = categorie.data
+  }
+
+  onBeforeMount( async ()=>{
+        await fetchAllCatego()
+     })
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
+  
