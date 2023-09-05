e6ccebbfc0b7106e02c9e4a662a25a02d9daf82c
bwikp <zilpa.michel@gmail.com>
2023-09-05 21:53:57 +0200

    regex sécure

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,6 +10,8 @@ const password = ref('')
 const router = useRouter()
 
 
+const regexMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+const regexPassword = /^.{6,}$/
 const LoginCheck = async () => {
   const loginInfo = {
     username:email.value,
@@ -28,8 +30,8 @@ const LoginCheck = async () => {
 //   console.log(token.data.token)
   localStorage.setItem('token',token.data.token)
   console.log(localStorage.token)
-}
 
+}
 
 </script>
 
@@ -38,11 +40,13 @@ const LoginCheck = async () => {
     <h2>Login</h2>
     <div class="loginZone">
       <label for="email">email</label>
-      <input type="text" name="email" placeholder="email" v-model="email" required>
+      <input type="text" name="email" @change="console.log(regexMail.test(email))" placeholder="email" v-model="email"  />
+      <p v-if="!regexMail.test(email) && email != '' ">please enter a valid mail adress !!!</p>
       <label for="password">password</label>
-      <input type="password" name="password" placeholder="password" v-model="password" required>
+      <input type="password" name="password" placeholder="password" v-model="password" >
       <RouterLink class="linkLogin" to="/register">Sign in</RouterLink>
-      <input class="DwdButton" type="button" value="login" @click="LoginCheck()">
+      <p v-if="!regexPassword.test(password) && password != '' "> At least 6 characters</p>
+      <input  v-if="regexMail.test(email) == true  && regexPassword.test(password) == true " class="DwdButton" type="button" value="login" @click="LoginCheck()">
     </div>
   </div>
 </template>
@@ -55,5 +59,8 @@ const LoginCheck = async () => {
   .loginZone{
     color: black;
   }
-  
+  .loginZone p
+    {
+      color: red;
+    }
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -9,6 +9,9 @@ const first_name = ref('')
 const last_name = ref('')
 const router = useRouter()
 
+const regexMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+const regexPassword = /^.{6,}$/
+
 const registerUser = async () => {
     const infoUser = {
         email: email.value,
@@ -29,15 +32,17 @@ const registerUser = async () => {
         <h2>Register</h2>
         <div class="RegisterZone">
             <label for="email">email</label> 
-            <input type="email" name="email" placeholder="email" required="required" v-model="email">
+            <input type="email" name="email" placeholder="email" r v-model="email" />
+            <p v-if="!regexMail.test(email) && email != '' ">please enter a valid mail adress !!!</p>
             <label for="password">password</label>
-            <input type="password" name="password" placeholder="password" required="required" v-model="password">
+            <input type="password" name="password" placeholder="password"  v-model="password">
+            <p v-if="!regexPassword.test(password) && password != '' "> At least 6 characters</p>
             <label for="fname">first name</label>
-            <input type="text" name="fname" placeholder="first_name" required="required" v-model="first_name">
+            <input type="text" name="fname" placeholder="first_name"  v-model="first_name">
             <label for="lname">last name</label>
-            <input type="text" name="lname" placeholder="last_name"  required="required" v-model="last_name">
+            <input type="text" name="lname" placeholder="last_name"  v-model="last_name">
             <RouterLink class="linkRegister" to="/login">Sign up</RouterLink>
-            <input id="DwdRegister" type="button" value="register"  @click="registerUser()">
+            <input v-if="regexMail.test(email) == true  && regexPassword.test(password) == true " id="DwdRegister" type="button" value="register"  @click="registerUser()">
         </div>
     </div>
 </template>
@@ -46,6 +51,10 @@ const registerUser = async () => {
     color: black;
     margin-left: 50%;
     text-decoration: underline;
+  }
+
+  .RegisterZone p{
+    color: red;
   }
   
 </style>
