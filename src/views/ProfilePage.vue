f3b7bbcf03893dfb3e1c30b122567ce9ac4bdcf3
bwikp <zilpa.michel@gmail.com>
2023-09-10 02:38:47 +0200

    page profile contact ajouté

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -37,6 +37,38 @@ import jwt_decode from "jwt-decode";
     })
 </script>
 <template>
-        <div>{{ dataUser.last_name }}</div>
-        <div>TON PROFILE</div> 
+    <div class="pageProfile">
+        <div>{{ dataUser.last_name }} {{ dataUser.first_name }}</div>
+    </div>
+    <div class="footer">
+       <div class="ulbiss">
+            <a :href='dataUser.contact.twitter' target="_blank">twitter</a>
+            <a :href="dataUser.contact.github" target="_blank">github</a>
+            <a :href="dataUser.contact.linkedin" target="_blank">linkedin</a>
+       </div>
+    </div>
 </template>
+<style scoped>
+        .pageProfile{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 80%;
+            width: 100%;
+        }
+        .ulbiss{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            gap: 20px;
+            width: 100%;
+        }
+        .footer{
+            display: flex;
+            flex-direction:row;
+            align-items: center;
+            justify-content: center;
+        }
+</style>
